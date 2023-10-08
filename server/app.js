77b10ceb652794314a33cb50471a19d9d09062d0
Đặng Trần Tiến <tiendtfx22706@funix.edu.vn>
2023-10-08 12:04:04 +0700

    update cookie option

--- server/app.js
+++ server/app.js
@@ -65,7 +65,7 @@ app.use(
     saveUninitialized: false,
     store: store,
     cookie: {
-      // sameSite: 'none',
+      sameSite: false,
       // secure: true,
       expires: Date.now() * 1000 * 60 * 60 * 24 * 3,
       maxAge: 1000 * 60 * 60 * 24 * 3,
